51b67f7d18c704fc5cca1c0548af7c10c5dc1e2d
Christian Matyas <christian.matyas@lifbi.de>
2023-01-27 20:59:14 +0100

    feat: Add semantic structure
    Exercise: Setting up the HTML document

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+export default function Nav() {
+    return ( 
+        <nav>
+            <ul>
+                <li>Nav1</li>
+                <li>Nav2</li>
+                <li>Nav3</li>
+            </ul>
+        </nav>
+    )
+}
